b23c32c3e24241a0c924abf08f6f8c60e47b44fe
ddgvv <33596297+ddgvv@users.noreply.github.com>
2017-11-12 21:05:03 +0530

    Update onecount.py

--- onecount.py
+++ onecount.py
@@ -1,4 +1,4 @@
-l=list(map(int,input().split(' ')))
-for i in l:
-    if(l.count(i)==1):
-        print(i)
+k=list(map(int,input().split(' ')))
+for j in k:
+    if(k.count(j)==1):
+        print(j)
