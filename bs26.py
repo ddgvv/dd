5b97b97d72b06cba39fa8a16e62b2fb4a42cfef3
ddgvv <33596297+ddgvv@users.noreply.github.com>
2018-01-18 17:53:30 +0530

    Create bs26.py

---
+++ bs26.py
@@ -0,0 +1,11 @@
+a,b=input().split(' ')
+a,b=int(a),int(b)
+l=[]
+for num in range(a+1,b):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           l.append(num)
+print(" ".join(str(x) for x in l))
