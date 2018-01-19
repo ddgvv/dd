5805614910ef4cfc2845ed69701e6daf7ae58fbe
ddgvv <33596297+ddgvv@users.noreply.github.com>
2018-01-19 15:17:19 +0530

    Update bsp69.py

--- bsp69.py
+++ bsp69.py
@@ -1,3 +1,2 @@
-#69th problem
-if abs(map(int,input("Enter Two values").split(' ')))%2==0:print("even")
+if sum(list(map(int,input("Enter Two values").split(' '))))%2==0:print("even")
 else:print("odd")
