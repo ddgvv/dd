ed24db5ea1f3ea05741776ee170c64ea11e04e99
ddgvv <33596297+ddgvv@users.noreply.github.com>
2018-02-01 08:26:44 +0530

    Update p9.py

--- p9.py
+++ p9.py
@@ -1,10 +1,10 @@
-lower,upper=map(int,(input().split(' ')))
+lo,up=map(int,input().split(' '))
+l=list(1 for i in range(up+1))
 c=0
-for num in range(lower,upper + 1):
-   if num > 1:
-       for i in range(2,num):
-           if (num % i) == 0:
-               break
-       else:
-           c+=1
+for i in range(2,up+1,1):
+    if l[i]==1:
+        for j in range(i*i,up+1,i):
+            l[j]=0
+        if i>=lo:
+            c+=1
 print(c)
