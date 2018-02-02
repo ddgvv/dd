55b9c2fa82f180ac102f209abd0286e10633e99a
ddgvv <33596297+ddgvv@users.noreply.github.com>
2018-02-02 14:00:06 +0530

    Create p21.py

---
+++ p21.py
@@ -0,0 +1,5 @@
+x1,y1=map(int,input("Enter !st line").split(' '))
+x2,y2=map(int,input("Enter 2nd line").split(' '))
+x3,y3=map(int,input("Enter 3rd line").split(' '))
+if (x1==x2 and x2==x3) or(y1==y2 and y2==y3) or (abs(x1-x2)==abs(x2-x3) and abs(y1-y2)==abs(y2-y3)):print("yes")
+else:print("no")
