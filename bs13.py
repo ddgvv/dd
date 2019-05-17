a5c828b9df7e74e4a6f9f6943cf9db2262641453
ddgvv <33596297+ddgvv@users.noreply.github.com>
2019-05-17 13:06:07 +0530

    Update bs13.py

--- bs13.py
+++ bs13.py
@@ -1,4 +1,5 @@
 letter=input()
 vowel=['a','e','i','o','u','A','E','I','O','U']
-if letter in vowel: print("Vowel")
+if not letter.isalpha():print('invalid')
+elif letter in vowel: print("Vowel")
 else: print("Consonant")
